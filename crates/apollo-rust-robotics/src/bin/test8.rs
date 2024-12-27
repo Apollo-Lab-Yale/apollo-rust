405553a27fa4b009ecd2a74b56b09d241362dda8
djrakita <djrakita@gmail.com>
2024-12-27 15:07:39 -0500

    wip

--- crates/apollo-rust-robotics/src/bin/test8.rs
+++ crates/apollo-rust-robotics/src/bin/test8.rs
@@ -1,10 +1,14 @@
 use apollo_rust_modules::ResourcesRootDirectory;
-use apollo_rust_robotics::ToChainNalgebra;
+use apollo_rust_modules::robot_modules::urdf_module::{ApolloURDFAxis, ApolloURDFJointLimit, ApolloURDFJointType, ApolloURDFPose};
+use apollo_rust_preprocessor::ResourcesSubDirectoryTrait;
+use apollo_rust_preprocessor::robot_modules_preprocessor::{AttachmentPoint, CombinedRobot};
 
 fn main() {
     let r = ResourcesRootDirectory::new_from_default_apollo_robots_dir();
-    let c = r.get_subdirectory("ur5").to_chain_nalgebra();
+    let s = r.get_subdirectory("d1");
 
-    let v: Vec<f64> = c.urdf_module().joints.iter().map(|x| x.limit.velocity).collect();
-    println!("{:?}", v);
+    CombinedRobot::new("go2d1_floating")
+        .attach_robot("go2d1", AttachmentPoint::World, ApolloURDFJointType::Floating, ApolloURDFPose::identity(), ApolloURDFAxis::default(), ApolloURDFJointLimit::default(), None, None)
+        .create_and_preprocess(&r)
+
 }
