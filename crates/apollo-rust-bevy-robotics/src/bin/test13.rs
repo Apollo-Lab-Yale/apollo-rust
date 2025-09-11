a2862e7f7ecef4acb96e07266bfb747a291f3ce6
djrakita <djrakita@gmail.com>
2025-09-11 15:19:37 -0400

    wip

--- crates/apollo-rust-bevy-robotics/src/bin/test13.rs
+++ crates/apollo-rust-bevy-robotics/src/bin/test13.rs
@@ -1,10 +1,13 @@
 use apollo_rust_bevy::ApolloChainBevyTrait;
+use apollo_rust_linalg::{ApolloDVectorTrait, V};
 use apollo_rust_modules::ResourcesRootDirectory;
 use apollo_rust_robotics::{PreprocessForceBuildModulesTrait, ToChainNalgebra};
 
 fn main() {
     let r = ResourcesRootDirectory::new_from_default_apollo_robots_dir();
-    let s = r.get_subdirectory("d1");
+    let s = r.get_subdirectory("ur5");
     let c = s.to_chain_nalgebra();
-    c.bevy_display();
+
+    // println!("{:?}", c.fk(&V::new(&[0.0])));
+    c.refine_link_shapes_skips_module();
 }
--- crates/apollo-rust-preprocessor/src/bin/test2.rs
+++ crates/apollo-rust-preprocessor/src/bin/test2.rs
@@ -3,13 +3,11 @@ use apollo_rust_modules::robot_modules::link_shapes_modules::link_shapes_skips_m
 use apollo_rust_modules::robot_modules::mesh_modules::original_meshes_module::ApolloOriginalMeshesModule;
 use apollo_rust_modules::robot_modules::mesh_modules::plain_meshes_module::ApolloPlainMeshesModule;
 use apollo_rust_preprocessor::{PreprocessorModule, ResourcesSubDirectoryTrait};
+use apollo_rust_preprocessor::robot_modules_preprocessor::CombinedRobot;
 
 fn main() {
     let r = ResourcesRootDirectory::new_from_default_apollo_robots_dir();
-    let s = r.get_subdirectory("d1");
+    let s = r.get_subdirectory("openarm");
 
-    // s.preprocess_robot(false);
-
-    ApolloPlainMeshesModule::load_or_build(&s, true);
-    // ApolloLinkShapesSkipsModule::load_or_build(&s, true).expect("error");
+    s.preprocess_robot(false);
 }
