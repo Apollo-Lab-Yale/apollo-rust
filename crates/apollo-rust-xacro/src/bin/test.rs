ba159b5c1b9e481d635da0bdb46ae4596b158a2a
djrakita <djrakita@gmail.com>
2025-12-27 12:16:26 -0500

    wip

--- crates/apollo-rust-xacro/src/bin/test.rs
+++ crates/apollo-rust-xacro/src/bin/test.rs
@@ -12,8 +12,8 @@ fn main() {
     );
     let output_path = PathBuf::new_from_desktop_dir().append("test.urdf");
 
-    let mut args = HashMap::new();
-    args.insert("ur_type".to_string(), "ur5".to_string());
+    // let mut args = HashMap::new();
+    // args.insert("ur_type".to_string(), "ur5".to_string());
 
-    process_xacro_to_file(&xacro_path, &[dir], &output_path, Some(&args)).expect("error");
+    process_xacro_to_file(&xacro_path, &[dir], &output_path, None).expect("error");
 }
